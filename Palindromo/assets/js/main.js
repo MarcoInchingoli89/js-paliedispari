de26d7bcd913435652e93d1f36004e8eb1e1a5c4
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-14 11:59:27 +0200

    Add first instructions

--- Palindromo/assets/js/main.js
+++ Palindromo/assets/js/main.js
@@ -2,3 +2,14 @@
 
 /* Palindroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+// Usiamo prompt per chiedere all'utente di inserire una parola
+//Inseriamo prompt in una costante
+const userWord = prompt('Inserisci una parola');
+/* console.log(userWord); */
+//Impostiamo il parametro della funzione con la nostra variabile di input
+Palindroma(userWord);
+//Creiamo una funzione
+function Palindroma(word) {
+    console.log(userWord);
+}
