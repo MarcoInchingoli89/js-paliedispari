235eca8b37de48f0bcfeb41f05db7f952ee41a36
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-14 12:31:24 +0200

    Add commento parole palindrome

--- Palindromo/assets/js/main.js
+++ Palindromo/assets/js/main.js
@@ -6,10 +6,15 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 // Usiamo prompt per chiedere all'utente di inserire una parola
 //Inseriamo prompt in una costante
 const userWord = prompt('Inserisci una parola');
+// Per verificare se una parola è palindroma controllare se una parola è uguale sia avanti che indietro
+
 /* console.log(userWord); */
 //Impostiamo il parametro della funzione con la nostra variabile di input
-Palindroma(userWord);
+const wordOutput = Palindroma(userWord);
 //Creiamo una funzione
 function Palindroma(word) {
-    console.log(userWord);
+    //Misuriamo la lunghezza delle parole usando lenght sul nostro input
+    //Mettiamo la misura in una nuova costante
+    const wordLenght = userWord.length;
+    console.log(wordLenght);
 }
