3ac01ac60c0446dff5740274c27a174ac529eb2c
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-14 15:45:48 +0200

    Add prompt and first function

--- Pari e Dispari/assets/js/main.js
+++ Pari e Dispari/assets/js/main.js
@@ -5,20 +5,32 @@
 /* Istruzioni */
 
 //Chiedere con un prompt se l'utente sceglie pari o dispari
+prompt ('Giocatore scegli pari o dispari');
 //Chiedere con un prompt numerico un numero da 1 a 5
 //Impostare una variabile
+const playerNumber = Number (prompt ('Ora scegli un numero da 1 a 5'));
+//Inserire la variabile di entrata in una costante per inserirla come paramentro nella funzione
+const isEvenOrOdd = evenOdd(playerNumber);
 //Creare una funzione
-//Nella funzione generiamo un numero casuale per il computer con math floor e math random
-//Lo impostiamo in una variabile
-//Facciamo la somma dei due numeri in uscita
-//Mettiamo la somma in una variabile
+function evenOdd (numb) {
+    //Nella funzione generiamo un numero casuale per il computer con math floor e math random
+    //Lo impostiamo in una variabile
+    const cpuNumber = Math.floor(Math.random() *5);
+    //Facciamo la somma dei due numeri in uscita
+    //Mettiamo la somma in una variabile
+    const sum = playerNumber + cpuNumber;
+    //Preleviamo il valore con return, ci servirà nella seconda funzione
+    return sum;
+}
+
 //Creare una seconda funzione
 //Prendiamo la somma contenuta in una variabile e verifichiamo se è pari o dispari
 //Per vedere se è pari o dispari usiamo l'operatore modulo
 //In un ciclo If se la somma è divisibile per 2 è pari
 //In un ciclo If se la somma non divisibile per 2 è dispari
-//Preleviamo il valore con return
+
 //Crea un ciclo If
 //Se il risultato ottenuto è uguale all'input del giocatore, il giocatore è il vincitore
 //Se il risultato è diverso il vincitore è il computer
+//Altrimenti è un pareggio
 //A seconda del risultato stampiamo su console il vincitore con console.log
