5cb67a775cf0c5d8f9438a12edfc52614b1e227b
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-14 15:14:16 +0200

    Add Istruzioni Pari e Dispari

--- Pari e Dispari/assets/js/main.js
+++ Pari e Dispari/assets/js/main.js
@@ -2,3 +2,23 @@
 
 /* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
+/* Istruzioni */
+
+//Chiedere con un prompt se l'utente sceglie pari o dispari
+//Chiedere con un prompt numerico un numero da 1 a 5
+//Impostare una variabile
+//Creare una funzione
+//Nella funzione generiamo un numero casuale per il computer con math floor e math random
+//Lo impostiamo in una variabile
+//Facciamo la somma dei due numeri in uscita
+//Mettiamo la somma in una variabile
+//Creare una seconda funzione
+//Prendiamo la somma contenuta in una variabile e verifichiamo se è pari o dispari
+//Per vedere se è pari o dispari usiamo l'operatore modulo
+//In un ciclo If se la somma è divisibile per 2 è pari
+//In un ciclo If se la somma non divisibile per 2 è dispari
+//Preleviamo il valore con return
+//Crea un ciclo If
+//Se il risultato ottenuto è uguale all'input del giocatore, il giocatore è il vincitore
+//Se il risultato è diverso il vincitore è il computer
+//A seconda del risultato stampiamo su console il vincitore con console.log
